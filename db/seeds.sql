d95a6e201f56322cd43079bd52d21c0bd7f18e21
Stevtm <steventmaddison@outlook.com>
2021-05-16 20:00:49 -0400

    update seeds for demo

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,21 +9,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES 
     ("Sales Lead", 100000.00, 1), 
     ("Salesperson", 65000.00, 1), 
-    ("Lead Engineer", 130000.00, 2), 
-    ("Software Engineer", 80000.00, 2), 
-    ("Account Manager", 85000.00, 3), 
+    ("Lead Engineer", 150000.00, 2), 
+    ("Software Engineer", 90000.00, 2), 
+    ("Account Manager", 100000.00, 3), 
     ("Accountant", 70000.00, 3), 
-    ("Legal Team Lead", 90000.00, 4);
+    ("Legal Team Lead", 110000.00, 4),
+    ("Lawyer", 90000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES 
-    ("Engineering", "Boss", 3, NULL), 
-    ("Engineer", "One", 4, 1), 
-    ("Engineer", "Two", 4, 1),
-    ("Sales", "Boss", 1, NULL), 
-    ("Salesperson", "One", 2, 4),
-    ("Salesperson", "Two", 2, 4),
-    ("Account", "Boss", 5, NULL), 
-    ("Accountant", "One", 6, 7),
-    ("Accountant", "Two", 6, 7),
-    ("Legal", "Boss", 5, NULL);
+    ("John", "Doe", 3, NULL),
+    ("Penny", "Tool", 4, 1), 
+    ("Nathaniel", "Down", 4, 1),
+    ("Valentino", "Morose", 1, NULL), 
+    ("Eric", "Widget", 2, 4),
+    ("Niles", "Peppertrout", 5, NULL), 
+    ("Sue", "Ameter", 6, 7),
+    ("Ingrid", "Tone", 5, NULL);
